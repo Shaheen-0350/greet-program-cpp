338a0bc604f7370554ee07cb473329d1d5b37bad
Shaheen-0350 <134725071+Shaheen-0350@users.noreply.github.com>
2023-12-31 11:33:02 +0530

    Update greet.cpp

--- greet.cpp
+++ greet.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::string name;
+    std::string name, response;
+    int status=0;
     std::cout << "What's your full name: ";
     std::getline(std::cin, name);
 
     std::cout << "Hello " << name << "!\n"; 
+    do {
+        std::cout << "Is the weather good?:(y/n) ";
+        std::cin >> response;
+        if(response=='y'||response=='Y') {
+            std::cout << "That's good!\n";
+            status=1;
+        }
+        else if(response=='n'||response=='N')
+            std::cout << "Sorry to hear that.\n";
+            status=1;
+        else {
+            std::cout << "Oops wrong input\n";
+            std:: << "Let's do this again.\n";
+            std::cout << std::endl;
+        }}
+    while(status=0);
 
     return 0;
 }
