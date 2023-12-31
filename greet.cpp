566d74af7c9c542cb1340df98e309aae88894c5f
Shaheen-0350 <shaheen1412@outlook.com>
2023-12-31 11:52:27 +0530

    Asks about the weather and uses input validation

--- greet.cpp
+++ greet.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::string name, response;
+    std::string name;
+    std::string response;
     int status=0;
     std::cout << "What's your full name: ";
     std::getline(std::cin, name);
 
     std::cout << "Hello " << name << "!\n"; 
-    do {
+    while(status==0) {
         std::cout << "Is the weather good?:(y/n) ";
         std::cin >> response;
-        if(response=='y'||response=='Y') {
+        if(response =="y"||response=="Y") {
             std::cout << "That's good!\n";
             status=1;
         }
-        else if(response=='n'||response=='N')
+        else if(response=="n"||response=="N") {
             std::cout << "Sorry to hear that.\n";
             status=1;
+        }
         else {
             std::cout << "Oops wrong input\n";
-            std:: << "Let's do this again.\n";
+            std::cout << "Let's do this again.\n";
             std::cout << std::endl;
-        }}
-    while(status=0);
+        }
+        }
 
     return 0;
 }
